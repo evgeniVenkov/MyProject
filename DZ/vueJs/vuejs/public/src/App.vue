6a545f121ff030a4cbc3bacd07aa3c5b874ce9b7
evgeniVenkov <evgenijvenkov183@gmail.com>
2024-06-15 19:06:42 +0300

    add vue

---
+++ DZ/vueJs/vuejs/public/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return{
+      films:[]
+    }
+
+  },
+}
+
+</script>
+
+<template>
+ <h1>Страница с фильмами
+ </h1>
+</template>
+
+<style scoped>
+@import "./assets/base.css";
+</style>
