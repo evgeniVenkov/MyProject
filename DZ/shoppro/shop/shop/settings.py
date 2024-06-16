6b1fb58781e5ca19d9c1801c20688df7c6b220a8
evgeniVenkov <evgenijvenkov183@gmail.com>
2024-06-16 18:38:50 +0300

    add new app

--- DZ/shoppro/shop/shop/settings.py
+++ DZ/shoppro/shop/shop/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -37,6 +38,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+
 ]
 
 ROOT_URLCONF = 'shop.urls'
